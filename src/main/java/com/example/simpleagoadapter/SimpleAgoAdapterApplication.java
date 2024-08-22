b475ab48577f57d80e13f03568d0d24da4c7e080
MikhailKurotchenko <shaurmanaaviamotornoy@gmail.com>
2024-08-22 11:59:52 +0300

    Added text in method main

--- src/main/java/com/example/simpleagoadapter/SimpleAgoAdapterApplication.java
+++ src/main/java/com/example/simpleagoadapter/SimpleAgoAdapterApplication.java
@@ -10,6 +10,7 @@ public class SimpleAgoAdapterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleAgoAdapterApplication.class, args);
+        System.out.println("ТЕСТ");
     }
 
 }
